a73ef72801008e73eda3f55285667d91fbdf5155
Levi Kilcher <Levi.Kilcher@gmail.com>
2018-08-27 14:56:56 -0600

    Add atlantic projection.

--- proj.py
+++ proj.py
@@ -5,6 +5,11 @@ pc = crs.PlateCarree()
 proj = {}
 proj['wc'] = crs.AlbersEqualArea(
     #central_longitude=-124.74, central_latitude=48.38,  # ~Cape Flattery
-    central_longitude= -124.41, central_latitude=40.44,  # ~Cape Mendocino
+    central_longitude=-124.41, central_latitude=40.44,  # ~Cape Mendocino
     standard_parallels=[32, 49]
+)
+
+proj['at'] = crs.AlbersEqualArea(
+    central_longitude=-75.52, central_latitude=35.22,  # ~Cape Hatteras
+    standard_parallels=[25, 44]
 )
