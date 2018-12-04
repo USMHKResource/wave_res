784fe15d808e2331c67889ae5034ba9195543071
Levi Kilcher <levi.kilcher@gmail.com>
2018-12-04 10:15:39 -0700

    working on script...

--- extract_land.py
+++ extract_land.py
@@ -9,6 +9,26 @@ scale = '50m'
 land = cfeature.NaturalEarthFeature('physical', 'land', scale,
                                     edgecolor='face', facecolor=cfeature.COLORS['land'])
 
+
+def overlap(r0, r1):
+    return (within(r0[0], r1) or within(r0[1], r1) or
+            within(r1[0], r0) or within(r1[1], r0))
+
+
+def within(val, r):
+    return (r[0] < val) & (val < r[1])
+
+   
+def plot_lines(rinf, ax, **kwargs):
+    eez_kw = kwargs.pop('eez_kw', {})
+    kwargs.update(transform=proj.pc)
+    for cid in rinf.con_defs:
+        tmp = kwargs.copy()
+        if cid == 'eez':
+            tmp.update(eez_kw)
+        for ll in rinf.get_lonlat(cid):
+            ax.plot(ll[0], ll[1], **tmp)
+
 region = 'wc'
 
 prj = proj.proj[region]
@@ -20,14 +40,12 @@ feat = ax.add_feature(land)
 ax.add_feature(cfeature.STATES.with_scale(scale))
 ax.set_extent((prj.lonlim + prj.latlim), crs=proj.pc)
 
+rinf = base.RegionInfo(region, use_old_con_defs=False)
+prj = proj.proj[region]
+ax.set_extent((prj.lonlim + prj.latlim), crs=proj.pc)
 
-def overlap(r0, r1):
-    return (within(r0[0], r1) or within(r0[1], r1) or
-            within(r1[0], r0) or within(r1[1], r0))
-
-
-def within(val, r):
-    return (r[0] < val) & (val < r[1])
+plot_lines(rinf, ax, color='m', 
+           eez_kw=dict(lw=3))
 
 cons = []
 
@@ -43,3 +61,5 @@ for idx, d in enumerate(land.geometries()):
             cons.append(lldat)
 
             ax.plot(lldat[0], lldat[1], '.', transform=proj.pc)
+
+    
