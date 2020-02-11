9bcb23ffa6dbc9dbdc053232a128057e2d8efd13
Levi Kilcher <levi.kilcher@gmail.com>
2020-02-11 10:49:45 -0700

    Fix paths.

--- wave_res/grid_data.py
+++ wave_res/grid_data.py
@@ -1,23 +1,22 @@
-import os
 import cPickle as _pkl
 import gzip as gzip
 from paths import pkgdir
 
-Dir = os.getcwd()+'\\grid_data\\'
+GDDir = pkgdir / 'grid_data/'
 
-with open(Dir+'Contour_Ranges2.pkl', 'r') as fl:
+with open(str(GDDir / 'Contour_Ranges2.pkl'), 'r') as fl:
     con_defs = _pkl.load(fl)
-with open(Dir+'Contour_Ranges.pkl', 'r') as fl:
+with open(str(GDDir / 'Contour_Ranges.pkl'), 'r') as fl:
     con_defs_old = _pkl.load(fl)
-with open(Dir+'GridLonLat.pkl', 'r') as fl:
+with open(str(GDDir / 'GridLonLat.pkl'), 'r') as fl:
     gridlonlat = _pkl.load(fl)
-with open(Dir+'FreqBins.pkl', 'r') as fl:
+with open(str(GDDir / 'FreqBins.pkl'), 'r') as fl:
     freqbins = _pkl.load(fl)
-with open(Dir+'LandData.pkl', 'r') as fl:
+with open(str(GDDir / 'LandData.pkl'), 'r') as fl:
     land_data = _pkl.load(fl)
-with open(Dir+'Boundaries.pkl', 'r') as fl:
+with open(str(GDDir / 'Boundaries.pkl'), 'r') as fl:
     bounds = _pkl.load(fl)
-with gzip.open(Dir+'DiffTriangles.pkl.gz', 'r') as fl:
+with gzip.open(str(GDDir / 'DiffTriangles.pkl.gz'), 'r') as fl:
     tri_defs = _pkl.load(fl)
 
 
