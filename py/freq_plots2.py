3b747f4027f866836a7cc7ad4383f9b6e75f69fa
Levi Kilcher <levi.kilcher@gmail.com>
2020-06-10 11:38:20 -0600

    Clean-up the script.

--- py/freq_plots2.py
+++ py/freq_plots2.py
@@ -67,14 +67,6 @@ def spec_freq2period(spec, fbins, norm=True):
     
 if flag['show remote']:
 
-    figI = plt.figure(101)
-    figI.clf()
-    figI, axI = plt.subplots(1, 1, num=figI.number)
-
-    figI2 = plt.figure(103)
-    figI2.clf()
-    figI2, axI2 = plt.subplots(1, 1, num=figI2.number)
-
     fig0 = plt.figure(102)
     fig0.clf()
     fig0, ax0 = plt.subplots(1, 1, num=fig0.number)
@@ -83,40 +75,11 @@ if flag['show remote']:
 
         dat = pdh5.load(str(srcdir / '{case}/{region}.{type}-totals.h5'.format(case='baseline', region=region, type='remote')))
 
-        df = np.diff(dat['fbins'])
-        f = dat['fbins'][:-1] + df / 2
-
-        spec_freq2period(dat['1way'][:, :, -1].mean(0), dat['fbins'])
-        
-        Tbins = dat['fbins'] ** -1
-        Tbins_center = Tbins[:-1] + np.diff(Tbins) / 2
-        Fbins = 1. / Tbins
-        dT = np.diff(Tbins)
-
-
-        dnow = dat['1way'][:,:,-1].mean(0) * df / 1e9
-        #dnow = np.hstack(([0], dnow, [0]))
-        #f2 = np.hstack(([1. / 30], f, [0.5]))
-
-        Int = np.cumsum(dnow[::-1])[::-1]
-
-        axI.plot(1. / f, Int)
-        Int2 = np.interp(Fbins, f, Int)
-        axI.plot(1. / Fbins, Int2, '+')
-
-        # Transform to period-space
-        dnow2 = dat['1way'][:, :, -1].mean(0) * f ** 2
-        dnow2 /= -(dnow2 * dT).sum()
-
-        dnow2, Tcent = spec_freq2period(dat['1way'][:, :, -1].mean(0), dat['fbins'])
-        
-        axI2.plot(Tcent, dnow2,
-                  label=labels[region],
-                  color=colors[region])
-
-        d_plot = np.diff(Int2) / Int2[0] / np.diff(Tbins)
-        
-        ax0.plot(Tbins_center, d_plot,
+        # Grab the edge of the EEZ, and take a time average
+        dnow = dat['1way'][:, :, -1].mean(0)
+        specT, T_center = spec_freq2period(dnow, dat['fbins'])
+                
+        ax0.plot(T_center, specT,
                  color=colors[region],
                  label=labels[region])
         
@@ -130,10 +93,10 @@ if flag['show remote']:
     ax0.set_xticks(np.arange(0, 31, 5))
     #ax0.xaxis.grid(True)
     ax0.legend()
-    ax0.set_ylabel('Energy Distribution [1/s]')
+    ax0.set_ylabel('Normalized Energy Distribution [1/s]')
     ax0.set_title("Remote Resource")
     ax0.set_xlabel("Wave Period [s]")
-    #ax0.set_ylim(remote_ylim)
+    ax0.set_ylim([0, 0.2])
     fig0.savefig('../fig/RemoteResource_Freq{}.pdf'.format(tag))
     fig0.savefig('../fig/RemoteResource_Freq{}.png'.format(tag), dpi=300)
 
@@ -145,10 +108,6 @@ ls = {'baseline': '-', 'extraction': ':'}
 
 if flag['show local']:
 
-    figI = plt.figure(201)
-    figI.clf()
-    figI, axI = plt.subplots(1, 1, num=figI.number)
-
     fig0 = plt.figure(202)
     fig0.clf()
     fig0, ax0 = plt.subplots(1, 1, num=fig0.number)
@@ -158,32 +117,18 @@ if flag['show local']:
         for case in ['baseline', 'extraction']:
             dat = pdh5.load(str(srcdir / '{case}/{region}.{type}-totals.h5'.format(case=case, region=region, type='local')))
 
-            df = np.diff(dat['fbins'])
-            f = dat['fbins'][:-1] + df / 2
 
-            Tbins = dat['fbins'] ** -1
-            Tbins_center = Tbins[:-1] + np.diff(Tbins) / 2
-            Fbins = 1. / Tbins
+            # Sum over the EEZ, average in time
+            dnow = dat['stot'][:, :, :].sum(-1).mean(0)
 
-
-            dnow = dat['stot'][:,:,-1].mean(0) * df / 1e9
-            #dnow = np.hstack(([0], dnow, [0]))
-            #f2 = np.hstack(([1. / 30], f, [0.5]))
-
-            Int = np.cumsum(dnow[::-1])[::-1]
-
-            axI.plot(1. / f, Int)
-            Int2 = np.interp(Fbins, f, Int)
-            axI.plot(1. / Fbins, Int2, '+')
-
-            d_plot = np.diff(Int2) / Int2[0] / np.diff(Tbins)
-
+            specT, T_center = spec_freq2period(dnow, dat['fbins'])
+            
             if case == 'baseline':
                 label = labels[region]
             else:
                 label = None
 
-            ax0.plot(Tbins_center, d_plot,
+            ax0.plot(Tbins_center, specT,
                      color=colors[region],
                      label=label,
                      linestyle=ls[case]
@@ -206,7 +151,7 @@ if flag['show local']:
     ax0.xaxis.grid(True)
     ax0.axhline(0, linestyle=':', color='k')
     ax0.set_title('Local Resource')
-    ax0.set_ylabel('[GW]')
+    ax0.set_ylabel('Normalized Energy Distribution [1/s]')
     ax0.set_xlabel('Wave Period [s]')
     ax0.legend()
     #ax0.set_ylim(local_ylim)
