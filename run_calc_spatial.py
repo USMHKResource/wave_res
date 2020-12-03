22d46e91cd550ca02d96096bd31a09c2ac29650b
Levi Kilcher <levi.kilcher@gmail.com>
2020-12-02 21:40:05 -0700

    Cleanup the run_calc_spatial script.

--- run_calc_spatial.py
+++ run_calc_spatial.py
@@ -4,16 +4,26 @@ from wave_res import paths
 import pyDictH5 as pdh5
 
 run_these = ['ak', 'hi', 'wc', 'at', 'prusvi']
+#run_these = ['ak']
+#run_these = ['hi']
 #run_these = ['wc']
+#run_these = ['at']
+#run_these = ['prusvi']
 
 months = np.arange(np.datetime64('1979-01'), np.datetime64('2011-01'))
-#months = np.arange(np.datetime64('2009-01'), np.datetime64('2009-03'))
+###months = np.arange(np.datetime64('2009-01'), np.datetime64('2009-03'))
 
 outdir = paths.tmpdir / 'baseline_spatial'
 
 paths.mkdir(str(outdir))
 
+print("This job is process these regions:")
+print(run_these)
+print('------------- Begin Loop ----------------')
+
 for region in run_these:
+
+    print("Processing data for region: {}".format(region))
 
     _tmp = cr.load_processed('baseline', region, np.datetime64('2009-01'))
     N_ang = len(_tmp['direction'])
@@ -30,11 +40,8 @@ for region in run_these:
     out['time'] = months
 
     for imo, month in enumerate(months):
-
+        print(month)
         dnow = cr.load_processed('baseline', region, month)
         out['wef'][:, imo] = (dnow['wef'] * np.diff(dnow['fbins'])[None, :, None]).sum(1)
 
     out.to_hdf5(str(outdir / 'ww3.{}.wef_spatial.h5'.format(region)))
-
-
-#ld = pdh5.load(str(outdir / 'ww3.wc.wef_spatial.h5'))
